c9ed7d50b46078d9dffb093603d745be761a5f99
Cristian Carrillo <cristiancarrillo@Mac-mini-de-Cristian.local>
2024-07-20 21:13:18 -0400

    add changes

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,73 @@
+import { Activity } from "../types"
+
+export type ActivityActions =
+    { type: 'save-activity', payload: { newActivity: Activity } } |
+    { type: 'save-activeId', payload: { id: Activity['id'] } } |
+    { type: 'delete-activity', payload: { id: Activity['id'] } } |
+    { type: 'restart-app' }
+
+export type ActivityState = {
+    activities: Activity[],
+    activeId: Activity['id']
+}
+
+const localStirageActivities = (): Activity[] => {
+    const activities = localStorage.getItem('activities')
+    return activities ? JSON.parse(activities) : []
+}
+
+export const initialState: ActivityState = {
+    activities: localStirageActivities(),
+    activeId: ''
+}
+
+export const activityReducer = (
+    state: ActivityState = initialState,
+    action: ActivityActions
+) => {
+
+    // esta accion maneja la logica
+    if (action.type === 'save-activity') {
+
+        let updateActivities: Activity[] = []
+        if (state.activeId) {
+            updateActivities = state.activities.map(activity => activity.id === state.activeId ? action.payload.newActivity : activity)
+
+        } else {
+            updateActivities = [...state.activities, action.payload.newActivity]
+        }
+
+        return {
+            ...state,
+            activities: updateActivities,
+            activeId: ''
+        }
+    }
+
+    if (action.type === 'save-activeId') {
+        return {
+            ...state,
+            activeId: action.payload.id
+        }
+
+    }
+
+    if (action.type === 'delete-activity') {
+        return {
+            ...state,
+            activities: state.activities.filter(activity => activity.id !== action.payload.id)
+        }
+
+    }
+
+    if (action.type === 'restart-app') {
+        return {
+            activities: [],
+            activeId: ''
+        }
+
+    }
+
+    return state
+
+}
